f6e70fd7bbcd2938448a40f68237e93eb0f9fd70
Fazbrid <odin.jose43000@gmail.com>
2023-04-17 10:41:20 -0400

    se agrego funciones al para agrgar a la pantalla y al boton borrar

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+let display = document.getElementById("display")
+
+let buttons = Array.from(document.querySelectorAll(".button"))
+
+
+buttons.map(button =>{
+    button.addEventListener('click',(e)=>{
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText= ''
+                break
+            case'↞':
+                display.innerText = display.innerText.slice(0,-1)
+                break
+            default:
+                display.innerText += e.target.innerText
+        }
+    })
+})
