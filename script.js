99e94c002874f4c859e245f25ac0e17e25a370c3
Fazbrid <odin.jose43000@gmail.com>
2023-04-18 17:54:56 -0400

    termino de l proyect

--- script.js
+++ script.js
@@ -14,13 +14,42 @@ function desactivar(){
     document.getElementById('multiplicacion').disabled = true;
     document.getElementById('dividir').disabled =true;
 }
+/* actia los botones de */
 function activar(){
     document.getElementById('suma').disabled = false;
     document.getElementById('resta').disabled = false;
     document.getElementById('multiplicacion').disabled = false;
     document.getElementById('dividir').disabled =false;
 }
-
+function transformar(numero){
+    let num1 = ''
+    let num2 = ''
+    let punto = true
+    if (numero.includes('.')){
+        for(let x = 0; x < numero.length; x++){
+            if (numero[x] === '.'){
+                punto = false
+            }
+            if(punto){
+                num1 += numero[x]
+            }else{
+                num2 += numero[x]
+            }
+            
+        }
+    }else{
+        return numero
+    }
+    num2 = num2.substring(1,num2.length)
+    num2 = parseInt(num2)
+    while (num2 >= 1) {
+        num2 = num2 / 10
+    }
+    numero = num2 + parseInt(num1)
+    return numero
+    
+}
+/* calcula las operaciones dependiendo su simbolo */
 function calcular(operador,num1,num2){
     
     let resultado = 0
@@ -79,12 +108,12 @@ buttons.map(button =>{
                     desactivar()
                 }
             case '+':
-
+                console.log(display.innerText)
                 if (display.innerText === ''){
                     break
                 }{
+                    anterior =transformar(display.innerText)
                     
-                    anterior = parseInt(display.innerText)
                     display.innerText = ''
                     simbolo = '+'
                     desactivar()
@@ -92,6 +121,8 @@ buttons.map(button =>{
                 break
             case 'C':
                 display.innerText = ''
+                anterior = 0
+                actual = 0
                 activar()
                 break
             case '↞':
@@ -104,7 +135,7 @@ buttons.map(button =>{
                 if (display.innerText=== ''){
                     actual = 0
                 }else{
-                    actual = parseInt(display.innerText)
+                    actual = transformar(display.innerText)
                 }
                 
                 display.innerText = calcular(simbolo,anterior ,actual);
